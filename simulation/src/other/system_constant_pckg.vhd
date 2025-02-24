e3938d76599c45904889cec2cd0a5945b2241c23
2523374362@qq.com <2523374362@qq.com>
2025-02-24 18:28:53 +0100

    need ccsds 121 and 123 parameter

--- simulation/src/other/system_constant_pckg.vhd
+++ simulation/src/other/system_constant_pckg.vhd
@@ -29,6 +29,12 @@ use std.textio.all;				-- for basic textio functions
 library shyloc_utils;
 use shyloc_utils.amba.all;        
 
+library shyloc_121;
+use shyloc_121.ccsds121_parameters.all;
+
+library shyloc_123; 
+use shyloc_123.ccsds123_parameters.all;
+
 package system_constant_pckg is
 
     -- AHB Master Input initialization
@@ -51,5 +57,33 @@ package system_constant_pckg is
         HMASTER    => (others => '0'),        -- 4-bit master identifier
         HMASTLOCK  => '0'                     -- Locked access signal
     );
+
+    -- Record type for CCSDS123 interface signals
+    type CCSDS123_Interface_Type is record
+        ForceStop      : std_logic;  -- Force the stop of the compression
+        AwaitingConfig : std_logic;  -- IP core waiting for configuration
+        Ready         : std_logic;  -- Ready to receive new samples
+        FIFO_Full     : std_logic;  -- Input FIFO is full
+        EOP           : std_logic;  -- Compression of last sample started
+        Finished      : std_logic;  -- IP finished compressing all samples
+        Error         : std_logic;  -- Error during compression
+    end record;
+    
+    -- Array type for the CCSDS123 interface record
+    type CCSDS123_Interface_Array_Type is array (natural range <>) of CCSDS123_Interface_Type;
+
+    -- Define array types for AHB interface signals
+    type AHB_Slv_In_Array_Type is array (natural range <>) of AHB_Slv_In_Type;
+    type AHB_Slv_Out_Array_Type is array (natural range <>) of AHB_Slv_Out_Type;
+    type AHB_Mst_In_Array_Type is array (natural range <>) of AHB_Mst_In_Type;
+    type AHB_Mst_Out_Array_Type is array (natural range <>) of AHB_Mst_Out_Type;
+    
+    -- Define array type for DataIn
+    type DataIn_Array_Type is array (natural range <>) of 
+        std_logic_vector(shyloc_123.ccsds123_parameters.D_GEN-1 downto 0);
+    
+    -- Define array type for DataOut
+    type DataOut_Array_Type is array (natural range <>) of 
+        std_logic_vector(shyloc_121.ccsds121_parameters.W_BUFFER_GEN-1 downto 0);
 
 end package system_constant_pckg;
