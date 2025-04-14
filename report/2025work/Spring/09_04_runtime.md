a5a7017fa0fed30d02d8ab9ebce703497bbdf89d
2523374362@qq.com <2523374362@qq.com>
2025-04-14 18:13:23 +0200

    add router_ahb_reg address 0x06

--- report/2025work/Spring/09_04_runtime.md
+++ report/2025work/Spring/09_04_runtime.md
@@ -4,5 +4,45 @@
 
 另外我使用router status register储存这个configuration register的话，我还需要将这个status register改为ahb 接口?也就是加上ahb slave接口
 
+实际configuration parameter update through Router port 0 from GR712, in port 0 there is a rmap_target.
 
-##
+
+## router_ahb_reg
+
+因为router\_config\_memory.vhd implements a configuration memory that can be both read from and written to
+
+while router_status_reg implements a status register that is read-only from the AXI interface.
+
+so it's reasonable to use router_config_memory instead of status reg
+
+```
+
+
+type r_maxi_lite_dword is record
+	taddr	: t_dword;
+	wdata 	: t_byte;
+	w_en	: std_logic;
+	tvalid	: std_logic;
+end record r_maxi_lite_dword;
+
+type r_saxi_lite_dword is record
+	rdata	: t_byte;
+	tready	: std_logic;
+end record r_saxi_lite_dword; ```
+
+router_port_0_controller 将output r_maxi_lite_dword 传输数据给 router_config_memory
+
+当axi_in.taddr(23 downto 16) = g_axi_addr , 也就是06 时 
+			if(axi_in.tvalid = '1' and axi_in.taddr(23 downto 16) = g_axi_addr) then				-- handshake request and valid address for this module ?
+					is_valid 		<= '1';															-- valid high ? (used for register preload)
+					wr_en 			<= axi_in.w_en;						    						-- load write status 
+				end if;
+```
+
+## router_port_0_controller_v2
+
+原router_port_0_controller Modul Address is from 0x00 to 0x05
+
+add router_ahb_reg address 0x06             address Space size 32x6
+
+-----from compression core AHB offset 0x00-0x14
--- simulation/src/shyloc_config/router_ahb_reg.vhd
+++ simulation/src/shyloc_config/router_ahb_reg.vhd
@@ -7,7 +7,7 @@
 --      <Revision number>: <Date>: <Comments>
 --      <Revision number>: <Date>: <Comments>
 --
--- Description: This register module is derived from router_status_reg and has been extended
+-- Description: This register module is derived from router_config_memory and has been extended
 --              with an AHB slave interface.
 --              AXI bus x"06"
 --
@@ -39,22 +39,20 @@ entity router_ahb_reg is
         haddr   : integer := 0;             --!Slave address
         hmask   : integer := 16#fff#;       --!Slave mask   
         -- register parameters        
-		g_addr_width 	: natural range 5 to 16 	:= 8;		-- limit 32-bit AXI address to these bits, default 6 == 64 memory elements
+		g_addr_width 	: natural range 4 to 16 	:= 5;		-- limit 32-bit AXI address to these bits, default 6 == 64 memory elements
 		g_axi_addr		: t_byte 					:= x"06"	-- axi Bus address for this module configure in router_pckg.vhd
 	);
 	port( 
-		
 		-- standard register control signals --
 		in_clk			: in 	std_logic 			:= '0';		-- clk input, rising edge trigger
 		out_clk			: in 	std_logic 			:= '0';		-- clk input, rising edge trigger
-		out_rst			: in 	std_logic 			:= '0';		-- reset input, active high
+		in_rst			: in 	std_logic 			:= '0';		-- reset input, active high
 		
 		-- AXI-Style Memory Read/wr_enaddress signals from RMAP Target
-		axi_in 			: in 	r_maxi_lite_dword	:= c_maxi_lite_dword; 	-- wdata is unused as this is a read only module 
+		axi_in 			: in 	r_maxi_lite_dword	:= c_maxi_lite_dword;
 		axi_out			: out 	r_saxi_lite_dword	:= c_saxi_lite_dword;
 		
-		status_reg_in	: in 	t_byte_array(0 to (2**g_addr_width)-1) := (others => (others => '0'))	-- status register inputs 
-		
+		config_mem_out	: out 	t_byte_array(0 to 31) := (others => (others => '0'))
     );
 end router_ahb_reg;
 
@@ -62,8 +60,10 @@ end router_ahb_reg;
 ---------------------------------------------------------------------------------------------------------------------------------
 -- Code Description & Developer Notes --
 ---------------------------------------------------------------------------------------------------------------------------------
--- c_num_stat_reg is stored in router.router_pckg(.vhd) library file. Top Constant Declarations....
-
+/*
+	contains read/write registers 	0  to 31 == Port Control Registers, 
+									32 to 35 == System Config Registers
+*/
 
 architecture rtl of router_ahb_reg is
 
@@ -79,63 +79,92 @@ architecture rtl of router_ahb_reg is
 	-- Function Declarations --
 	----------------------------------------------------------------------------------------------------------------------------
 	
+	
+	----------------------------------------------------------------------------------------------------------------------------
+	-- Component Declarations --
+	----------------------------------------------------------------------------------------------------------------------------
+	
 	----------------------------------------------------------------------------------------------------------------------------
 	-- Signal Declarations --
 	----------------------------------------------------------------------------------------------------------------------------
-	signal axi_addr 	: integer range 0 to (2**g_addr_width)-1 := 0;	-- convert axi address LSByte to integer
-	signal rd_data		: t_byte := (others => '0');
-	signal status_reg 	: t_byte_array(0 to (2**g_addr_width)-1) := (others => (others => '0'));
-	signal is_valid		: std_logic := '0';		-- flag reg used to track valid assertions. 
+	signal config_mem 	: t_byte_array(0 to (2**g_addr_width)-1) 	:= (others => (others => '0'));	-- create config memory 
+	signal axi_addr 	: natural range 0 to (2**g_addr_width)-1 	:= 0;								-- address is for both read & write 
+	signal rd_data		: t_byte 									:= (others => '0');
+	signal wr_data		: t_byte 									:= (others => '0');
+	signal wr_en 		: std_logic 								:= '0';
+	signal is_valid		: std_logic									:= '0';
 	
-	signal sync_reg_2	: t_byte_array(0 to (2**g_addr_width)-1) := (others => (others => '0'));
-	signal sync_reg_1	: t_byte_array(0 to (2**g_addr_width)-1) := (others => (others => '0'));
+	signal sync_reg_2		:  	t_byte_array(0 to (2**g_addr_width)-1) 	:= (others => (others => '0'));	-- create config memory 
+	signal sync_reg_1		: 	t_byte_array(0 to (2**g_addr_width)-1) 	:= (others => (others => '0'));	-- create config memory 
 	----------------------------------------------------------------------------------------------------------------------------
 	-- Variable Declarations --
 	----------------------------------------------------------------------------------------------------------------------------
 
-
+	----------------------------------------------------------------------------------------------------------------------------
+	-- Attribute Declarations --
+	----------------------------------------------------------------------------------------------------------------------------
 	
 begin
 
-
+	----------------------------------------------------------------------------------------------------------------------------
+	-- Entity Instantiations --
+	----------------------------------------------------------------------------------------------------------------------------
+	
+	----------------------------------------------------------------------------------------------------------------------------
+	-- Component Instantiations --
+	----------------------------------------------------------------------------------------------------------------------------
 	
 	----------------------------------------------------------------------------------------------------------------------------
 	-- Asynchronous Signal Assignments --
 	----------------------------------------------------------------------------------------------------------------------------
-	
+--	config_mem_out <= config_mem(0 to c_num_config_reg-1);		-- register outputs...
+--	axi_out.rdata   <= config_mem(axi_addr);			-- read back byte at axi address byte 
 	----------------------------------------------------------------------------------------------------------------------------
 	-- Synchronous Processes --
 	----------------------------------------------------------------------------------------------------------------------------
-	axi_proc: process(out_clk)
-	begin
-		if(rising_edge(out_clk)) then
-			axi_out.tready <= '0';	-- de-assert axi ready 
-			is_valid <= '0';		-- de-assert valid target flag
-			
-			if(axi_in.tvalid = '1' and axi_in.taddr(23 downto 16) = g_axi_addr) then
-				is_valid 	<= '1';			-- assert that target is valid for this address. 
-				axi_addr 	<= to_integer(unsigned(axi_in.taddr(g_addr_width-1 downto 0)));	-- get register address	
-			end if;
-			
-			if(axi_in.tvalid = '1' and is_valid = '1') then
-				axi_out.tready <= '1';			-- assert ready now read data will be ready 
-				axi_out.rdata <= status_reg(axi_addr);
-			end if;
-			
-			if(axi_out.tready = '1' and axi_in.tvalid = '1') then
-				axi_out.tready <= '0';		-- de-assert handshake
-				is_valid <= '0';			-- de-assert proload 
-			end if;
-			status_reg(0 to (2**g_addr_width)-1) <= sync_reg_2; 
-		end if;
-	end process;
-	-- ports write to their own status register, no need for WR address on Ports..
-
-	sync_proc: process(in_clk)
+	-- handles AXI handshake and registers IO with config port 
+	-- router clock domain 
+	axi_proc: process(in_clk)
 	begin
 		if(rising_edge(in_clk)) then
-			sync_reg_1 <= status_reg_in;
-			sync_reg_2 <= sync_reg_1;
+			if(in_rst = '1') then
+				axi_out.tready 	<= '0';									-- force de-assert ready..
+				is_valid 		<= '0';			
+			--	config_mem 		<= (others => (others => '0'));
+			else
+				axi_out.tready <= '0';
+				is_valid <= '0';
+				wr_data 		<= axi_in.wdata;					-- load write data to register
+				axi_out.rdata   <= config_mem(axi_addr);			-- read back byte at axi address byte 	
+				axi_addr 		<= to_integer(unsigned(axi_in.taddr(g_addr_width-1 downto 0)));	-- get requested address
+				
+				if(axi_in.tvalid = '1' and axi_in.taddr(23 downto 16) = g_axi_addr) then				-- handshake request and valid address for this module ?
+					is_valid 		<= '1';															-- valid high ? (used for register preload)
+					wr_en 			<= axi_in.w_en;						    						-- load write status 				
+				end if;
+				
+				if(is_valid = '1' and axi_in.tvalid = '1') then				-- bus target valid ?
+					axi_out.tready 	<= '1';									-- assert handshake ready 
+				end if;			
+
+				if(axi_out.tready = '1' and axi_in.tvalid = '1') then		-- axi handshake asserted ?
+					axi_out.tready 	<= '0';									-- force de-assert ready..
+					is_valid 		<= '0';									-- de-assert pre-load 
+				end if;
+				
+				if(wr_en = '1')then											-- write enable asserted ?
+					config_mem(axi_addr) <= wr_data;						-- write config byte to element
+				end if;
+			end if;
+		end if;
+	end process;
+	
+	-- dual register outputs in spacewire clock domain 
+	out_proc: process(out_clk)
+	begin
+		if(rising_edge(out_clk)) then
+			sync_reg_1 <= config_mem;
+			config_mem_out <= sync_reg_1;
 		end if;
 	end process;
 
